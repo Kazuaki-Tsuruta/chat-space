aca6b751dae74724bb4b2c5d0f05640aeb105465
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-05-27 15:59:18 +0900

    revise for 10-6

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,11 +1,18 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_info
+  
   def index
-    @messages = Message.all
-    @group = Group.find(params[:group_id])
-
     respond_to do |format| 
       format.html 
       format.json { @new_messages = @group.messages.where("id > ?", params[:id]) }
     end
   end
+
+  private
+  
+  def set_info
+    @messages = Message.all
+    @group = Group.find(params[:group_id])
+  end
+
 end
